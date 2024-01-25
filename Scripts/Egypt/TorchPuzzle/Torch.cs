df425f6bc36eab647f669b746abac93501e6cbe3
Jokin Irizar Melero <joirizar@ceinpro.es>
2024-01-25 15:30:03 +0100

    torch puzzle implemented, gotta hide the objects.

--- Scripts/Egypt/TorchPuzzle/Torch.cs
+++ Scripts/Egypt/TorchPuzzle/Torch.cs
@@ -3,31 +3,22 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
-    public string listenToTag = "Comp";
     public bool IsFull = false;
-    public string CorrectItem;
-    private GameObject father;
     private int Count = 0;
-    public Door Door;
 
     public delegate void EventHandler(string current);
 
     public static event EventHandler OnComplete;
 
-    public void Awake()
-    {
-        father = transform.parent.gameObject;
-        Debug.Log(transform.parent.name + " " + " " + CorrectItem);
-    }
     private void Place(Rigidbody rb, GameObject Item)
     {
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = Vector3.zero;
-        rb.rotation = Quaternion.identity;
-        Item.transform.position = transform.position;
-        Item.layer = LayerMask.NameToLayer("Default");
-        Item.GetComponent<BoxCollider>().enabled = false;
-        Item.GetComponent<Rigidbody>().useGravity = false;
+        GameManager.instance.IsObjectPickedUp = false;
+        Destroy(Item);
+        transform.GetChild(Count-1).gameObject.GetComponent<MeshRenderer>().enabled = true;
+        if(Count == transform.childCount)
+        {
+            transform.GetComponent<BoxCollider>().enabled = false;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -35,23 +26,23 @@ public class Torch : MonoBehaviour
         //Comprobar tag de objeto
         GameObject Item = other.gameObject;
         string ItemName = Item.name;
-        Debug.Log("OnTrigger");
 
         //Comprobar si el objeto a snappear es el correcto en base a su name y el valor de count (0 palos, 1 bandeja, 2 leña)
         if (!IsFull)
         {
-            if (Item.CompareTag("Torch"))
-            {
                 Rigidbody rb = other.GetComponent<Rigidbody>();
                 if (ItemName == "Legs" && Count == 0)
                 {
-                    Place(rb, Item);
+                  
                     Count++;
                     Debug.Log("Patas colocadas");
                     if (Item.GetComponent<PickableItem>())
                     {
                         Item.GetComponent<PickableItem>().IsPlaced = true;
                     }
+                   
+
+                    Place(rb, Item);
                 }
                 else if (ItemName == "Plate" && Count == 1)
                 {
@@ -63,19 +54,23 @@ public class Torch : MonoBehaviour
                         Item.GetComponent<PickableItem>().IsPlaced = true;
                     }
                 }
-                else if (ItemName == "Wood" && Count == 2)
+                else if ((ItemName == "Firewood" && Count >1)&&(Count< transform.childCount))
                 {
-                    Place(rb, Item);
-                    IsFull = true;
+                Count++;
+                Place(rb, Item);
+                    
                     Debug.Log("Leña colocada");
-                    Door.Activation = true;
                     if (Item.GetComponent<PickableItem>())
                     {
                         Item.GetComponent<PickableItem>().IsPlaced = true;
                     }
-                    OnComplete("");
-                }
+                   
+                }else if (Count == transform.childCount)
+            {
+                IsFull = true;
+                //OnComplete("");
             }
+            
         }
     }
 }
